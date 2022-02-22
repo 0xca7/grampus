0cec4332db0951fb1b74bc95ba91abac7dfd5bde
0xca7 <m1etz@protonmail.com>
2022-02-22 19:03:57 +0100

    copying

--- src/corpus.rs
+++ src/corpus.rs
@@ -1,4 +1,22 @@
 /*
+    grampus - a crappy grammar fuzzer
+    Copyright (C) 2022  0xca7
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/*
     Description:
         the corpus for fuzzing.
         this consists of multiple syntax trees
